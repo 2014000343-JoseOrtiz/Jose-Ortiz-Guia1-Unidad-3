5c0a03b1637986afe615ddd8067eed312c1236ba
2014000343-JoseOrtiz <j.ortiz.5cdb@gmail.com>
2026-06-16 20:06:10 -0600

    Add files via upload

---
+++ Lecc16_Act2_Inc1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] edades = new int[7];
+        int cantidadMayoresEdad = 0;
+
+        for (int i = 0; i < 7; i++)
+        {
+            Console.Write("Ingrese la edad de la persona " + (i + 1) + ": ");
+            edades[i] = Convert.ToInt32(Console.ReadLine());
+
+            if (edades[i] >= 18)
+            {
+                cantidadMayoresEdad++;
+            }
+        }
+
+        Console.WriteLine("Cantidad de personas mayores de edad: " + cantidadMayoresEdad);
+        Console.ReadKey();
+    }
+}
